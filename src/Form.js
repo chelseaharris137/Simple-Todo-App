3fa6919c90744df6dfa3211776e4d0a0bbac7816
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2021-03-24 16:50:42 -0400

    added form component file

---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = {
+      todos: [],
+    };
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Form;
