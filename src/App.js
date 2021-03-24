a85b9de8c7a47a6c875d08df3ce3f9b7fbdc6a39
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2021-03-24 16:21:57 -0400

    created and implemented todo component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import Todos from "./Todos";
 
 class App extends Component {
   constructor(props) {
     super(props);
     // Don't call this.setState() here!
-    this.state = { counter: 0 };
+    this.state = {
+      todos: [
+        { id: 1, content: "Buy some milk" },
+        { id: 2, content: "Walk the dog" },
+      ],
+    };
   }
   //add a component did mount to fetch to the database
   render() {
-    return <div>HELLO WORLD!</div>;
+    return (
+      <div>
+        <Todos />
+      </div>
+    );
   }
 }
 
---
+++ src/Todos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Todos = (props) => {
+  return <div className="todos"></div>;
+};
+
+export default Todos;
